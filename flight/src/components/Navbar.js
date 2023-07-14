cbb50489cba6e04219e28d2af0cb3562533c3503
ajaythopate <ajaythopate999@gmail.com>
2023-07-14 16:23:02 +0530

    signin sign out  added

--- flight/src/components/Navbar.js
+++ flight/src/components/Navbar.js
@@ -3,20 +3,24 @@ import { SiConsul } from "react-icons/si";
 import { BsPhoneVibrate } from "react-icons/bs";
 import { AiOutlineGlobal } from "react-icons/ai";
 const Navbar = () => {
-  return (
-      <div className="navbar flex">
-          <div className="navBarOne flex">
-              <div>
-                  <SiConsul />
-              </div>
-              <div className="none flex">
-                  <li className="flex"> <BsPhoneVibrate /> Support</li>
-                  <li className="flex"> <AiOutlineGlobal /> Languages</li>
+    return (
+        <div className="navbar flex">
+            <div className="navBarOne flex">
+                <div>
+                    <SiConsul />
+                </div>
+                <div className="none flex">
+                    <li className="flex"> <BsPhoneVibrate /> Support</li>
+                    <li className="flex"> <AiOutlineGlobal /> Languages</li>
 
-              </div>
-          </div>
-    </div>
-  )
+                </div>
+                <div className="atb flex">
+                    <span> Sign In</span>
+                    <span>Sign Out</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
