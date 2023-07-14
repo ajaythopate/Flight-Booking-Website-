4e34eead36479f560d16b66f63d505d4aa7cf02a
ajaythopate <ajaythopate999@gmail.com>
2023-07-14 16:19:57 +0530

    navbar list  added

--- flight/src/components/Navbar.js
+++ flight/src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { SiConsul } from "react-icons/si";
+import { BsPhoneVibrate } from "react-icons/bs";
+import { AiOutlineGlobal } from "react-icons/ai";
 const Navbar = () => {
   return (
       <div className="navbar flex">
           <div className="navBarOne flex">
               <div>
                   <SiConsul />
+              </div>
+              <div className="none flex">
+                  <li className="flex"> <BsPhoneVibrate /> Support</li>
+                  <li className="flex"> <AiOutlineGlobal /> Languages</li>
+
               </div>
           </div>
     </div>
