644e93e54f825b7faa3b007b0bdd4e8c73392889
ajaythopate <ajaythopate999@gmail.com>
2023-07-16 12:11:22 +0530

    home page h1 title added

--- flight/src/components/Home.js
+++ flight/src/components/Home.js
@@ -1,29 +1,29 @@
-// import React from 'react';
-// // import BgImge from "../assets/bg.jpg";
+import React from 'react';
+// import BgImge from "../assets/bg.jpg";
 // import PlaneImge from "../assets/plane.png";
 
-// const Home = () => {
-//   return (
-//     <div className="home flex container">
+const Home = () => {
+  return (
+    <div className="home flex container">
 
 
-//       <div className="mainText">
-//         <h1>Create Ever-Lasting Memories With Us</h1>
-//       </div>
+      <div className="mainText">
+        <h1>Create Ever-Lasting Memories With Us</h1>
+      </div>
 
-//       <div className="homebg flex">
+      {/* <div className="homebg flex">
 
-//         <div className="imgDiv">
+        <div className="imgDiv">
 
-//           {/* <img src={BgImge} alt="BgImge" className="img"></img> */}
-//         </div>
+          <img src={BgImge} alt="BgImge" className="img"></img>
+        </div>
 
-//         <img src={PlaneImge} alt="PlaneImg" className="planImg" ></img>
+        <img src={PlaneImge} alt="PlaneImg" className="planImg" ></img>
 
 
-//       </div>
-//     </div>
-//   )
-// }
+      </div> */}
+    </div>
+  )
+}
 
-// export default Home
+export default Home
