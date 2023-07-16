7076d2ff579401078264e798830f7649dfcde1e1
ajaythopate <ajaythopate999@gmail.com>
2023-07-16 12:12:49 +0530

    home page h1 image  added

--- flight/src/components/Home.js
+++ flight/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import BgImge from "../assets/bg.jpg";
-// import PlaneImge from "../assets/plane.png";
+import PlaneImge from "../assets/plane.png";
 
 const Home = () => {
   return (
@@ -11,17 +11,17 @@ const Home = () => {
         <h1>Create Ever-Lasting Memories With Us</h1>
       </div>
 
-      {/* <div className="homebg flex">
+       <div className="homebg flex">
 
         <div className="imgDiv">
 
-          <img src={BgImge} alt="BgImge" className="img"></img>
+          {/* <img src={BgImge} alt="BgImge" className="img"></img> */}
         </div>
 
         <img src={PlaneImge} alt="PlaneImg" className="planImg" ></img>
 
 
-      </div> */}
+      </div> 
     </div>
   )
 }
